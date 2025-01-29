f1f6b0285691f6823c6b51d65c5683856e49d20b
rushi <rushikeshdivase96s@gmail.com>
2025-01-29 12:06:41 +0530

    new

--- src/App.js
+++ src/App.js
@@ -1,28 +1,9 @@
 import React from 'react'
-import { BrowserRouter, Route, Routes } from 'react-router-dom'
-import AddEmployeeComponent from './components/AddEmployeeComponent'
-import EmployeeListComponent from './components/EmployeeListComponent'
-import Navbar from './components/Navbar'
-import UserRegister from './components/UserRegister'
-import Login from './components/Login'
-import PrivateComponent from './components/PrivateComponent'
 
 function App() {
   return (
     <div>
-      <BrowserRouter>
-        <Navbar />
-        <Routes>
-          <Route element={<PrivateComponent />}>
-            <Route path="/" element={<EmployeeListComponent />} />
-            <Route path="/add-emp" element={<AddEmployeeComponent />} />
-            <Route path="/update-emp/:id" element={<AddEmployeeComponent />} />
-          </Route>
-          
-          <Route path='/login' element={<Login />} />
-          <Route path='/register-user' element={<UserRegister />}></Route>
-        </Routes>
-      </BrowserRouter>
+<h1>Hello..! React Application is Deployed..!</h1>
     </div>
   )
 }
--- src/services/EmployeeService.js
+++
@@ -1,6 +0,0 @@
-import axios from "axios";
-
-const REST_BASE_API = "http://localhost:8088/emp-service/";
-
-export const createEmployee = (employee) => {axios.post(REST_BASE_API, employee);}
-
